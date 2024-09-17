7290efd8d64a1b76899f9147232aa7b4714b0677
mabdillahislamy <105412674+mabdillahislamy@users.noreply.github.com>
2024-09-17 15:27:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+< !--JavaScript to Handle Redirection-- >
+    <script>
+        document.getElementById('visitorForm').onsubmit = function() {
+            // Delay to ensure form is submitted before redirecting
+            setTimeout(function() {
+                window.location.href = 'https://c3e8acbf-2817-4d84-ba56-0d0f594284c1-00-o61rs43leh7z.sisko.replit.dev/'; // Ganti dengan URL tujuan Anda
+            }, 1000); // 1000 milliseconds = 1 second delay
+      };
+    </script>
